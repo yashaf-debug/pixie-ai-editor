14d08470150fb61818497fa037af4cedb160346a
yashaf-debug <radchenko.yakov@gmail.com>
2026-01-11 18:59:54 +0300

    fix: repair tailwind.config.js syntax error

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,6 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        gray: {
-          750: '#2d3748',
-          850: '#1a202c',
-          950: '#0d1117',
-        }
-      }
-    }
-  },
-  "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
         display: ['Montserrat', 'sans-serif'],
@@ -28,6 +15,11 @@ export default {
       colors: {
         electric: '#2563EB',
         dark: '#0A0A0A',
+        gray: {
+          750: '#2d3748',
+          850: '#1a202c',
+          950: '#0d1117',
+        }
       },
       backgroundImage: {
         'noise': "url('data:image/svg+xml,%3Csvg viewBox=\"0 0 200 200\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cfilter id=\"noiseFilter\"%3E%3CfeTurbulence type=\"fractalNoise\" baseFrequency=\"0.65\" numOctaves=\"3\" stitchTiles=\"stitch\"/%3E%3C/filter%3E%3Crect width=\"100%25\" height=\"100%25\" filter=\"url(%23noiseFilter)\" opacity=\"1\"/%3E%3C/svg%3E')",
